9a183593bc957cbc9fb40ca6854418c78d240d6e
forestanthonypro <forest.anthony.pro@ik.me>
2026-08-13 17:26:48 +0200

    feat: traduction des groupes Senat espagnol et Chambre italienne dans les 8 langues

--- apps/web/lib/partyNameLabels.js
+++ apps/web/lib/partyNameLabels.js
@@ -25,6 +25,23 @@ const PARTY_KEYS = {
   "Italia Viva - Casa Riformista": "party_it_iv",
   "Misto": "party_it_misto",
   "Per le Autonomie (SVP-PATT, Campobase)": "party_it_autonomie",
+  "GRUPO PARLAMENTARIO POPULAR EN EL SENADO": "party_es_senado_pp",
+  "GRUPO PARLAMENTARIO SOCIALISTA": "party_es_senado_psoe",
+  "GRUPO PARLAMENTARIO IZQUIERDAS POR LA INDEPENDENCIA (ESQUERRA REPUBLICANA-EUSKAL HERRIA BILDU)": "party_es_senado_erc_bildu",
+  "GRUPO PARLAMENTARIO PLURAL EN EL SENADO JUNTS PER CATALUNYA-COALICIÓN CANARIA-AGRUPACIÓN HERREÑA INDEPENDIENTE-BLOQUE NACIONALISTA GALEGO": "party_es_senado_plural",
+  "GRUPO PARLAMENTARIO VASCO EN EL SENADO (EAJ-PNV)": "party_es_senado_pnv",
+  "GRUPO PARLAMENTARIO IZQUIERDA CONFEDERAL (MÁS MADRID, EIVISSA I FORMENTERA AL SENAT, COMPROMÍS, AGRUPACIÓN SOCIALISTA GOMERA Y GEROA BAI)": "party_es_senado_confederal",
+  "GRUPO PARLAMENTARIO MIXTO": "party_es_senado_mixto",
+  "FRATELLI D'ITALIA (FDI)": "party_it_camera_fdi",
+  "PARTITO DEMOCRATICO - ITALIA DEMOCRATICA E PROGRESSISTA (PD-IDP)": "party_it_camera_pd",
+  "LEGA - SALVINI PREMIER (LEGA)": "party_it_camera_lega",
+  "FORZA ITALIA - BERLUSCONI PRESIDENTE - PPE (FI-PPE)": "party_it_camera_fi",
+  "MOVIMENTO 5 STELLE (M5S)": "party_it_camera_m5s",
+  "ALLEANZA VERDI E SINISTRA (AVS)": "party_it_camera_avs",
+  "AZIONE-POPOLARI EUROPEISTI RIFORMATORI-RENEW EUROPE (AZ-PER-RE)": "party_it_camera_azione",
+  "NOI MODERATI (NOI CON L'ITALIA, CORAGGIO ITALIA, UDC E ITALIA AL CENTRO)-MAIE-CENTRO POPOLARE (NM(N-C-U-I)M-CP)": "party_it_camera_noimoderati",
+  "ITALIA VIVA-CASA RIFORMISTA (IV-CR)": "party_it_camera_iv",
+  "MISTO (MISTO)": "party_it_camera_misto",
 };
 
 export function translatePartyName(name, t) {
